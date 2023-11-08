459089523ca03a3bf2a3946992c635fb337fbdc2
Leonardo Emanuele <manups4e@gmail.com>
2023-11-08 14:33:13 +0100

    Let's try again!

---
+++ FreeRoamProject.Shared/Core/Log/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FreeRoamProject.Shared.Core.Log
+{
+    public class DebugLog : Logger.Log
+    {
+        public new void Debug(string text)
+        {
+#if DEBUG
+            string text2 = $"{DateTime.Now:dd/MM/yyyy, HH:mm}";
+            string text3 = "-- [DEBUG] -- ";
+            string text4 = "^5";
+            CitizenFX.Core.Debug.WriteLine(text4 + text2 + " " + text3 + " " + text + ".^7");
+#endif
+        }
+    }
+}
