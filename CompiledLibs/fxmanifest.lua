97403b51f0563aa03d03413b06ff67ff37955148
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2023-11-22 11:19:11 -0800

    Post build scripts
    This way our server path's aren't undone because of Git

---
+++ CompiledLibs/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+loadscreen_manual_shutdown 'yes'
+clr_experimental_2021_12_31_use_sync_context 'on'
+fxevents_debug_mode '1'
+
+files {
+	--"html/**/*",
+    'Client/Newtonsoft.Json.dll',
+    'Client/ScaleformUI.dll',
+    'Client/FxEvents.Client.dll',
+}
+
+client_scripts{
+    "Client/pedComponentExtensions.lua",
+    "Client/FreeRoamProject.Client.net.dll"
+}
+
+server_script "Server/FreeRoamProject.Server.net.dll"
+
+--ui_page "html/index.html"
+--loadscreen 'html/loadingscreen/loading.html'
